d617acb390a62b543cdd9eea58a538c2fc889250
Aaron Mertzenich <mertzi52101@hotmail.com>
2016-12-21 00:06:10 -0600

    Additional scaffold edits
    Added time property to the objects stored in the index

--- src/bullet-time.js
+++ src/bullet-time.js
@@ -1,23 +1,23 @@
 class BulletTime {
     constructor() {
-        this.index = [];
-        this.freeId = 0;
+        this._index = [];
+        this._freeId = 0;
     }
 
     addEvent(time, callback) {
         // Adds an event to the timeline and returns its ID. Accepts an optional callback function as a parameter.
-        this.index.push({
-            id: this.freeId,
+        this._index.push({
+            id: this._freeId,
+            time: time,
             callback: callback
         });
-        this.freeId++;
-        return this.freeId - 1;
+        return this._freeId++;
     }
 
     removeEvent(id) {
         // Removes the event with the given ID from the timeline.
-        let result = this.index.filter((obj) => obj.id === id);
-        if (result) this.index.splice(this.index.indexOf(result), 1);
+        let result = this._index.filter((obj) => obj.id === id);
+        if (result) this._index.splice(this._index.indexOf(result), 1);
     }
 
     play() {
