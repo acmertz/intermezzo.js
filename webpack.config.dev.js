35f91533ee3769ece784dc8a24fc904e8c8d2c00
Aaron Mertzenich <aaron.mertzenich@gmail.com>
2019-06-08 09:43:02 -0500

    Use html-webpack-plugin when building demos

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,9 +1,22 @@
-const path = require('path');
+const path = require('path'),
+    { CleanWebpackPlugin } = require('clean-webpack-plugin'),
+    HtmlWebpackPlugin = require('html-webpack-plugin'),
+    ScriptExtHtmlWebpackPlugin = require('script-ext-html-webpack-plugin');
 
 module.exports = {
     mode: 'development',
     entry: './build.js',
     devtool: 'eval-source-map',
+    plugins: [
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            template: 'index.html',
+            inject: 'head'
+        }),
+        new ScriptExtHtmlWebpackPlugin({
+            defaultAttribute: 'defer'
+        })
+    ],
     output: {
         filename: 'intermezzo.js',
         path: path.resolve(__dirname, 'dist'),
